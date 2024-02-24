67919150ded8375425b4af4c1a760e2e310f966b
markerson7 <markersondflomo@gmil.com>
2024-02-24 11:58:43 +0200

    This is my first python Assignment

--- User_Input.py
+++ User_Input.py
@@ -2,7 +2,4 @@ name = input('Enter your name:')
 age = input('Enter your age:')
 location = input('Enter your location:')
 
-# name = name
-# age = age
-# location = location
 print("Hello,", name + "you are", age, "and you live", location +".")
