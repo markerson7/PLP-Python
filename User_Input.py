27b2b8e255a1ff1a7f531d81a344de0983ea432c
markerson7 <markersondflomo@gmil.com>
2024-02-24 11:35:13 +0200

    First commit

---
+++ User_Input.py
@@ -0,0 +1,8 @@
+name = input('Enter your name:')
+age = input('Enter your age:')
+location = input('Enter your location:')
+
+# name = name
+# age = age
+# location = location
+print("Hello,", name + "you are", age, "and you live", location +".")
