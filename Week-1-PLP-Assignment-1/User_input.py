858a8465b412b788989cf4dbe6e4b2d66f716763
markerson7 <markersondflomo@gmil.com>
2024-02-28 18:35:47 +0200

    PLP-Week-1-Assignment

--- Week-1-PLP-Assignment-1/User_input.py
+++ Week-1-PLP-Assignment-1/User_input.py
@@ -1,5 +1,5 @@
-name = input('Enter your name:')
-age = input('Enter your age:')
-location = input('Enter your location:')
+name = input('Please enter your name:')
+age = input('Please enter your age:')
+location = input('Please enter your location:')
 
 print("Your name is", name, "and you are age is", age, "You Live", location)
