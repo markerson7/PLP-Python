4f4e1d1a240c0e86d122ed389f84f8f4a4c6bb0d
markerson7 <markersondflomo@gmil.com>
2024-04-14 08:29:56 +0200

    Week 3 Assignment

---
+++ Week-3-PLP-Python-Assignment-3/calculate-discount.py
@@ -0,0 +1,17 @@
+def calculate_discount(price, discount_percent):
+    if discount_percent >= 20:
+        discount_amount = price * (discount_percent / 100)
+        final_price = price - discount_amount
+        return final_price
+    else:
+        return price
+
+original_price = float(input("Enter the original price of the item: "))
+discount_percent = float(input("Enter the discount percentage: "))
+
+final_price = calculate_discount(original_price, discount_percent)
+
+if final_price == original_price:
+    print("No discount applied. Final price:", final_price)
+else:
+    print("Final price after applying discount:", final_price)
